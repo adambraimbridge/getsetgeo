8eb622e063cd78a40936585a827b3ed092766198
adambraimbridge <adam@braimbridge.com>
2021-10-07 16:32:21 +0800

    Switched to npm; Cosmetic updates to chat component

--- components/chat/index.js
+++ components/chat/index.js
@@ -2,38 +2,33 @@ const Chat = () => {
 	return (
 		<>
 			<section>
-				<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
+				<div className="p-5 max-w-sm mx-auto my-3 bg-white rounded-xl shadow-md flex items-center space-x-4">
+					<div className="flex-shrink-0">
+						<img className="h-12 w-12" src="/img/letter.svg" alt="Ahoy Logo" />
 					</div>
 					<div>
-						<div class="text-xl font-medium text-black">ChitChat</div>
-						<p class="text-gray-500">You have a new message!</p>
-					</div>
-				</div>
-				<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
-					</div>
-					<div>
-						<div class="text-xl font-medium text-black">ChitChat</div>
-						<p class="text-gray-500">You have a new message!</p>
-					</div>
-				</div>
-				<div class="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
-					<div class="flex-shrink-0">
-						<img class="h-12 w-12" src="/img/logo.svg" alt="ChitChat Logo" />
-					</div>
-					<div>
-						<div class="text-xl font-medium text-black">ChitChat</div>
-						<p class="text-gray-500">You have a new message!</p>
+						<div className="text-xl font-medium text-black">Ahoy</div>
+						<p className="text-gray-500">Hello traveller!</p>
 					</div>
 				</div>
 			</section>
+
+			<div className="p-5 max-w-sm mx-auto my-3 bg-white rounded-xl shadow-md flex items-center space-x-4">
+				<div className="flex-shrink-0">
+					<img className="h-12 w-12" src="/img/letter.svg" alt="Ahoy Logo" />
+				</div>
+				<div>
+					<div className="text-xl font-medium text-black">Ahoy</div>
+					<p className="text-gray-500">Are you there?</p>
+				</div>
+			</div>
+
 			<section>
-				<form>
-					<input class="border border-transparent focus:outline-none focus:ring-2 focus:ring-purple-600 focus:border-transparent ..." />
-					<button class="bg-purple-600 hover:bg-purple-700 focus:outline-none focus:ring-2 focus:ring-purple-600 focus:ring-opacity-50 ...">Sign up</button>
+				<form className="flex w-full max-w-sm mx-auto my-3 space-x-3">
+					<input className="flex-1 appearance-none border border-transparent w-full py-2 px-4 bg-white text-gray-700 placeholder-gray-400 shadow-md rounded-lg text-base focus:outline-none focus:ring-2 focus:ring-yellow-600 focus:border-transparent" type="text" placeholder="..." />
+					<button className="flex-shrink-0 bg-yellow-200 text-white text-base font-semibold py-2 px-4 rounded-lg shadow-md hover:bg-yellow-100 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 focus:ring-offset-yellow-200" type="button">
+						<img className="h-12 w-12" src="/img/send.svg" alt="Send" />
+					</button>
 				</form>
 			</section>
 		</>
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,7 @@
 import Head from 'next/head'
 import Link from 'next/link'
+import Image from 'next/image'
+import sunset from '../public/img/sunset.png'
 
 const Index = () => {
 	return (
@@ -11,9 +13,8 @@ const Index = () => {
 			</Head>
 
 			<div className="mx-auto">
-				<div>
-					<img src="/img/sunset.png" className="rounded-lg" />
-				</div>
+				<Image src={sunset} alt="Silhouettes of yachts at sunset" placeholder="blur" objectFit="contain" layout="responsive" className="rounded-lg" />
+
 				<div className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
 					<h1 className="text-yellow-400 text-4xl tracking-tight font-extrabold sm:text-5xl md:text-6xl">Get Set Freo</h1>
 					<h2 className="text-2xl tracking-tight font-extrabold text-gray-900 sm:text-2xl md:text-3xl">The Fremantle Adventure</h2>
@@ -47,7 +48,7 @@ const Index = () => {
 					<div className="pt-6 text-center leading-6 font-bold sm:text-lg sm:leading-7">
 						<Link href="/adventures/fremantle">
 							<button type="button" className="font-bold w-full sm:w-auto flex-none bg-gray-50 text-cyan-600 hover:text-cyan-700 py-3 sm:px-6 border border-gray-200 rounded-xl flex items-center justify-center space-x-2 sm:space-x-4 focus:ring-2 focus:ring-offset-2 focus:ring-offset-white focus:ring-gray-300 focus:outline-none transition-colors duration-200">
-								Get Set, Geo! &rarr;
+								Ready? Get Set, Geo! &rarr;
 							</button>
 						</Link>
 					</div>
